e5d7f0d8c41ee858842eb65314d9ba0e7a54c594
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-07 11:09:33 -0600

    deploy server

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const students = require('./data/students.js')
+const cors = require("cors")
+app.use(cors());
+
+function getId(students, number) {
+    return students.filter(student => {
+        console.log(student.id)
+        if (student.id == number) {
+            return student;
+        } else {
+            return null
+        }
+    })[0];
+}
+
+app.get('/', (req, res, next) =>
+    res.status(200).json({
+        data: students
+    }))
+
+app.get('/:id', (req, res) => {
+    var record = getId(students, req.params.id);
+    if (record) {
+        res.status(200).json({
+            data: record
+        });
+    } else {
+        res.status(404).json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    }
+});
+
+app.listen(process.env.PORT || 3000);
